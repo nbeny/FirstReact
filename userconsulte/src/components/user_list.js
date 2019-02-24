9dbd684c2dffbbc92018d124c0ac37113b27e983
nbeny <nbeny@student.42.fr>
2019-02-24 11:07:47 +0100

    Ca marche

---
+++ userconsulte/src/components/user_list.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'React'
+
+class UserList import Component {
+    render () {
+        return (
+            <div>
+                <ul className="col-md-4">
+                    {
+                        this.props.
+                    }
+                </ul>
+            </div>
+        )
+    }
+}
