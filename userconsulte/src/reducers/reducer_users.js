5a615ae0a6ce752acca8141ba568c75b198ba892
nbeny <nbeny@student.42.fr>
2019-02-25 09:17:21 +0100

    clean

--- userconsulte/src/reducers/reducer_users.js
+++
@@ -1,9 +0,0 @@
-export default function(){
-
-    return [
-               {name:'Sarah Croche',id:1,role:'ROOT',active:'NON'},
-               {name:'Eléonore Labanquise',id:2,role:'USER',active:'OUI'},
-               {name:'Emma Karena',id:3,role:'USER',active:'NON'},
-               {name:'Jean Bonnot',id:4,role:'ADMIN',active:'OUI'},
-           ] 
-}
